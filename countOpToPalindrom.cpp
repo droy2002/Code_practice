6c9bbe22403c67ed186378538725f71cbdbe07c8
Didhiti Roy <86788901+droy2002@users.noreply.github.com>
2023-07-09 19:12:51 +0530

    Add files via upload

---
+++ countOpToPalindrom.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int minOpToPalindrome(vector<int>& arr) {
+    std::vector<int> tempArr = arr;  // Create a copy of the original array
+    int count = 0;
+    int i = 0;
+    int j = tempArr.size() - 1;
+
+    while (i < j) {
+        if (tempArr[i] == tempArr[j]) {
+            i++;
+            j--;
+        } else if (tempArr[i] < tempArr[j]) {
+            tempArr[i + 1] += tempArr[i];  // Merge the current and next element
+            i++;
+            count++;
+        } else {
+            tempArr[j - 1] += tempArr[j];  // Merge the current and previous element
+            j--;
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+
+    int operations = minOpToPalindrome(arr);
+
+    cout << "Minimum number of operations to make the array palindrome: " << operations << std::endl;
+
+    return 0;
+}
